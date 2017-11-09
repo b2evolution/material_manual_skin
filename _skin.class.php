395bc4c4df5a230e64ad83d65972bd96627744c3
yurabakhtin <yurybakh@gmail.com>
2017-11-09 18:01:36 +0300

    Update version to 7

--- _skin.class.php
+++ _skin.class.php
@@ -21,7 +21,7 @@ class material_manual_Skin extends Skin
 	 * Skin version
 	 * @var string
 	 */
-	var $version = '1.2.0';
+	var $version = '7.2.0';
 	
 	/**
 	 * Do we want to use style.min.css instead of style.css ?
