83cb392888b3ab3db44ee9e7b99de6f2dff13c55
Erwin Rommel H. Satingin <winskie@gmail.com>
2018-07-16 08:47:58 +0800

    version bump

--- _skin.class.php
+++ _skin.class.php
@@ -21,7 +21,7 @@ class material_manual_Skin extends Skin
 	 * Skin version
 	 * @var string
 	 */
-	var $version = '1.2.1';
+	var $version = '6.2.1';
 
 	/**
 	 * Do we want to use style.min.css instead of style.css ?
